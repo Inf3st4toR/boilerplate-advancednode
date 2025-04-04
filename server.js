d1954ec47100d30d7dffd0951907361c4f7bda5f
inf3st4tor <xcharaix@gmail.com>
2025-04-04 06:41:28 +0000

    Complete Authentification strategies

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ myDB(async (client) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please login",
+      showLogin: true,
     });
   });
 
@@ -48,6 +49,18 @@ myDB(async (client) => {
       done(null, doc);
     });
   });
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("index", { title: e, message: "Unable to connect to database" });
